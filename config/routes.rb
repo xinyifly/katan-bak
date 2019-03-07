bdd4c4f8989950f50e5efe25b4b315d776433b00
Zeyu Chen <xinyifly@live.cn>
2019-03-07 17:27:23 +0800

    chore: generate controller Candidates

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'candidates/index'
   resources :votes
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
---
+++ test/controllers/candidates_controller_test.rb
@@ -0,0 +1,9 @@
+require 'test_helper'
+
+class CandidatesControllerTest < ActionDispatch::IntegrationTest
+  test "should get index" do
+    get candidates_index_url
+    assert_response :success
+  end
+
+end
