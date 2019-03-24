1747006e46f28a131f320d8a7f282cf20f6c6769
Zeyu Chen <xinyifly@live.cn>
2019-03-25 06:30:52 +0800

    test: fix urls

--- test/controllers/candidates_controller_test.rb
+++ test/controllers/candidates_controller_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class CandidatesControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
-    get candidates_index_url
+    get candidates_url
     assert_response :success
   end
 
