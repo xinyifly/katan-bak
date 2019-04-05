b04eac3641cf673e8d269fdce2cf7efac16afd71
Zeyu Chen <xinyifly@live.cn>
2019-04-05 21:31:49 +0800

    chore: use postgresql

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2019_03_05_125306) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -25,12 +28,14 @@ ActiveRecord::Schema.define(version: 2019_03_05_125306) do
   end
 
   create_table "votes", force: :cascade do |t|
-    t.integer "voter_id"
-    t.integer "candidate_id"
+    t.bigint "voter_id"
+    t.bigint "candidate_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["candidate_id"], name: "index_votes_on_candidate_id"
     t.index ["voter_id"], name: "index_votes_on_voter_id"
   end
 
+  add_foreign_key "votes", "users", column: "candidate_id"
+  add_foreign_key "votes", "users", column: "voter_id"
 end
